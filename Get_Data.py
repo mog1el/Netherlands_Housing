e3008c2203c359d15eb3e5cb140681c8d80628aa
mog1el <m.mogielnicki@gmail.com>
2024-07-25 18:34:53 +0100

    Improving

--- Get_Data.py
+++ Get_Data.py
@@ -16,34 +16,38 @@ for url in urls:
     listings = soup.find_all('section', class_='listing-search-item')
 
     for listing in listings:
-        #city is the 3rd word of an address and I'm only interested in this data 
+        #the true price only the first word. Important to know that price is per month
+        price_whole = listing.find('div', class_='listing-search-item__price').get_text(strip=True)
+        price_parts = price_whole.split()
+        price = price_parts[0]
+        price = price.replace('€', '').replace(',', '')
+        prices.append(price)
+
+        #city is the 3rd word of an address and I'm only interested in this data
         address = listing.find('div', class_="listing-search-item__sub-title'").get_text(strip=True)
         address_parts = address.split()
         city = address_parts[2]
         cities.append(city)
 
-        #the true price only the first word. Important to know that price is per month
-        price_whole = listing.find('div', class_='listing-search-item__price').get_text(strip=True)
-        price_parts = price_whole.split()
-        price = price_parts[0]
-        prices.append(price)
-
-        area = listing.find('li', class_='illustrated-features__item--surface-area').get_text(strip=True)
+        areas_whole = listing.find('li', class_='illustrated-features__item--surface-area').get_text(strip=True)
+        areas_parts = areas_whole.split()
+        area = areas_parts[0]
         areas.append(area)
 
-        room = listing.find('li', class_='illustrated-features__item--number-of-rooms').get_text(strip=True)
+        room_whole = listing.find('li', class_='illustrated-features__item--number-of-rooms').get_text(strip=True)
+        room_parts = room_whole.split()
+        room = room_parts[0]
         rooms.append(room)
 
 #%%
-#Create a dataframe
+#Creating a dataframe and exporting it
 df = pd.DataFrame({
     'City': cities,
     'Price': prices,
     'Area': areas,
     'Rooms': rooms
 })
-
+df = df.drop(df[df.Price == 'Price'].index)
 print(df)
 
-#Export to CSV
-df.to_csv('apartments.csv')
+df.to_csv('apartments.csv', header = False, index = False)
